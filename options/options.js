8cc10003934e2a1c60bb9d93072268bde04d715d
Crowdbotics <team@crowdbotics.com>
2023-08-06 20:01:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wickitup-gardens-42695",
+  url: "https://wickitup-gardens-42695.botics.co",
+  api: "https://wickitup-gardens-42695.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
